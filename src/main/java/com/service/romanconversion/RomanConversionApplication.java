14d7f525977202c515d11526a659a84ed8d7a369
aemuser <sudhakarnnvph@gmail.com>
2021-09-12 21:37:53 -0400

    Updates

--- src/main/java/com/service/romanconversion/RomanConversionApplication.java
+++ src/main/java/com/service/romanconversion/RomanConversionApplication.java
@@ -3,8 +3,6 @@ package com.service.romanconversion;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/service/romanconversion/controller/RomanController.java
+++ src/main/java/com/service/romanconversion/controller/RomanController.java
@@ -1,9 +1,10 @@
 package com.service.romanconversion.controller;
-import com.service.romanconversion.model.Result;
 import com.service.romanconversion.service.RomanService;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,45 +13,61 @@ import org.springframework.validation.annotation.Validated;
 import javax.validation.ConstraintViolationException;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+
 
 /**
- * Controller Class that has all the mappings defined
+ * Controller Class that has the service endpoints and delegates request
  */
 @Slf4j
 @RestController
 @Validated
 @NoArgsConstructor
+@EnableCaching
 public class RomanController {
 
     @Autowired
     private RomanService romanService;
 
-    public void RomanController(final RomanService romanService) {
-        this.romanService = romanService;
+    /**
+     * getRomanValue - takes an input (integer), returns the corresponding Roman Numeral
+     * @param query input param
+     * @return ResponseEntity
+     */
+    @GetMapping(path = "/romannumeral")
+    @Cacheable("results")
+    public @ResponseBody ResponseEntity<Object> getRomanValue(final @RequestParam( name = "query")
+                                                          @Min(value = 1, message = "Input Param Must Be Greater" +
+                                                                  "Than or Equal to 1")
+                                                          @Max(value = 3999, message = "Input Param Must Be Less" +
+                                                                  "than or equal to 3999") int query) {
+        log.debug("Calling RomanService getRomanValueForInteger");
+        return new ResponseEntity<>(romanService.getRomanValueForInteger(query), HttpStatus.OK);
     }
 
     /**
-     * getRomanValue - takes an input (integer), returns the corresponding Roman Numeral
-     * @param query
-     * @return
+     * Handle this Exception, send back meaningful message
+     * @param exception ConstraintViolationException
+     * @return String errorMessage
      */
-    @GetMapping(path = "/romannumeral")
-    public @ResponseBody ResponseEntity getRomanValue(final @RequestParam( name = "query", required = true)
-                                                          @Min(value = 1, message = "Input Param Must Be More Than 1")
-                                                          @Max(value = 3999, message = "Input Param Must Be Less" +
-                                                                  "Than 3999") int query) {
-        log.debug("Calling RomanService getRomanValueForInteger");
-        return new ResponseEntity<Object>(romanService.getRomanValueForInteger(query), HttpStatus.OK);
-    }
-
-    @ExceptionHandler({ConstraintViolationException.class, Exception.class})
+    @ExceptionHandler({ConstraintViolationException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    private String handleConstraintViolationException(ConstraintViolationException e) {
-        log.error("Constraint Violation Exception ", e);
-        return "There has been an error while processing your request. Please verify that the input parameter is an " +
+    private String handleConstraintViolationException(ConstraintViolationException exception) {
+        log.error("RomanController ConstraintViolationException ::", exception);
+        return "Invalid Input. Please verify that the input parameter is an integer between 1 and 3999";
+    }
+
+    /**
+     * Handle this Exception, send back a meaningful message
+     * @param exception NumberFormatException
+     * @return String errorMessage
+     */
+    @ExceptionHandler({NumberFormatException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    private String handleNumberFormatException(NumberFormatException exception) {
+        log.error("RomanController NumberFormatException ::", exception);
+        return "There has been an error while processing your input. Please verify that the input parameter is an " +
                 "integer between 1 and 3999";
     }
 }
